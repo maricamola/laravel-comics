838b7fcace7cfff448ca57b65c537a710bcddfeb
maricamola <maricamola98@libero.it>
2023-06-05 17:43:39 +0200

    logo printed on page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,13 @@
 <header>
-    Header
+    <header>
+        <div class="logo">
+        <img src="/img/dc-logo.png" alt="Logo dc">
+    </div>
+
+    <nav>
+        <ul>
+
+        </ul>
+    </nav>
+    </header>
 </header>
